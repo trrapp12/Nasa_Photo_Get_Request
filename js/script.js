cabb82c34fa6ab02a14e3c64a0fb7e350dbd21c4
Trevor Rapp <trrapp@gmail.com>
2021-09-02 23:14:20 -0600

    adds display area for JSON object received, adds randomizer that will pick number between 1 and 10 to choose how many images to receive

--- js/script.js
+++ js/script.js
@@ -4,12 +4,21 @@
     var httpRequest;
     // testing log to make sure file is connecting
     console.log("line 3: entered script.js file");
+    // creates a random number between 1 and 100
+    var randInt = Math.floor((Math.random() * 11) + 1);
+    // creates count parameter
+    var countParam = "&count=" + randInt.toString();
+
     // initializes the api_key NOTE THIS IS A HUGE SECURITY RISK.  WELL, NOT REALLY SINCE THE API IS ONLY TO NASA TO GET PHOTOS, BUT THIS WHOLE REPOSITORY SHOULD BE SECURED LATER USING git-remote-gcrypt
     var api_key = "gQBHOwp1QgftmdeZ3bO3KTnskprBBUwud1vmWgRz";
-    var requestURL = "https://api.nasa.gov/planetary/apod?api_key=gQBHOwp1QgftmdeZ3bO3KTnskprBBUwud1vmWgRz";
+    var requestURL = "https://api.nasa.gov/planetary/apod?api_key=gQBHOwp1QgftmdeZ3bO3KTnskprBBUwud1vmWgRz" + countParam;
 
     // adds event listener to the button going to make the call
     var requestButton = document.getElementById('makeRequestButton').addEventListener('click', makeRequest);
+
+    function displayJSON (json) {
+      var displayJSON = document.getElementById('json-display').innerHTML = json
+    };
 
 
     function makeRequest() {
@@ -22,7 +31,7 @@
       } else {
         console.log("line 23: entering makeRequest() else statement");
         httpRequest.onreadystatechange = alertContents;
-        httpRequest.open('GET', "https://api.nasa.gov/planetary/apod?api_key=gQBHOwp1QgftmdeZ3bO3KTnskprBBUwud1vmWgRz", true);
+        httpRequest.open('GET', requestURL, true);
         httpRequest.send();
       }
     }
@@ -34,6 +43,7 @@
         if (httpRequest.status === 200) {
           console.log("line 35: passed second condition, 200 validation occurred.  status is: " + httpRequest.status)
           // SECOND CHECK: everything is good, continue
+          displayJSON(httpRequest.responseText);
           alert("success! we contacted NASA" + httpRequest.responseText);
 
         } else {
@@ -44,7 +54,11 @@
         console.log("line 44: failed alertContents() readyState condition. readyState is: " + httpRequest.readyState)
         // FAIL FIRST CHECK: there is a problem with the ready state
       }
+
+      return httpRequest.responseText;
     }
+
+
     // function to open the information
   });
 })();
